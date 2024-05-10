d280b5efc617b90cc099d9dced29bc01ce38b434
mel <gilsebastian019@gmail.com>
2024-05-09 20:18:38 -0500

    Se realiza la captura de datos del formulario, se re organiza el css para el objeto independiente, se crea el onChange y los arreglos vacios, además de crear un componente de información vacio para almacenar los datos capturados y se instalan las dependencias del router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <>
    <h1>aiuda</h1>
-   <Formulario></Formulario>
+   <Formulario />
     </>
   )
 }
--- src/layouts/auth/Formulario.jsx
+++ src/layouts/auth/Formulario.jsx
@@ -1,29 +1,76 @@
-import React from 'react'
+import './Formulario.css'
+import React, { useRef, useState } from 'react'
+import { info } from '../../database/Informacion'
 
 function Formulario() {
+  const resetForm = useRef()
+  const [getNombre, setNombre] = useState("")
+  const [getApellido, setApellido] = useState("")
+  const [getEdad, setEdad] = useState("")
+  const [getCorreo, setCorreo] = useState("")
+  const [getNota, setNota] = useState("")
+
+  //const [reEstudiantes, setEstudiantes] = useState([])
+
+function registrarEstudiantes(e) {
+  e.preventDefault();
+  if (getNombre !== "" &&
+      getApellido !== "" &&
+      getEdad !== "" &&
+      getCorreo !== "" &&
+      getNota !== ""
+  ){ const datos = {
+      nombre: getNombre,
+      apellido: getApellido,
+      edad: getEdad,
+      correo: getCorreo,
+      nota: getNota
+    };
+    
+    info.push([datos])
+    console.log(datos)
+    resetForm.current.reset()
+  }
+}
+
   return (
-    <form action="#" method="post">
+    <form ref={resetForm} onSubmit={(e)=>e.preventDefault()} action="#" method="post">
       <label>
         Nombre:
-        <input type="text" name="name" required/>
+        <input onChange={(e)=>{
+          setNombre(e.target.value)
+        }} type="text" name="name" placeholder='Nombre'required/>
       </label>
+
       <label>
         Apellido:
-        <input type="text" name="lastName" required/>
+        <input onChange={(e)=>{
+          setApellido(e.target.value)
+        }}type="text" name="lastName" placeholder='Apellido' required/>
       </label>
+
       <label>
         Edad:
-        <input type="number" name="age" required/>
+        <input onChange={(e)=>{
+          setEdad(e.target.value)
+        }} type="number" name="age" placeholder='Edad' required/>
       </label>
+
       <label>
         Correo Electrónico:
-        <input type="email" name="email" required/>
+        <input onChange={(e)=>{
+          setCorreo(e.target.value)
+        }} type="email" name="email" placeholder='Correo' required/>
       </label>
+
       <label>
         Nota del Curso:
-        <input type="number" name="grade" required/>
+        <input onChange={(e)=>{
+          setNota(e.target.value)
+        }} type="number" name="grade" placeholder='Nota' required/>
       </label>
-      <button type="submit">Registrar</button>
+
+      <button onClick = {registrarEstudiantes} type="submit">Registrar</button>
     </form>
   )
 }
